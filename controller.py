9d9aa4e958bc44f97070760c011351fde5706302
Shawn Lee <dashawn@gmail.com>
2011-12-18 19:36:23 -0800

    Initial setup of MVC

---
+++ controller.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class Paper(object):
+  pass
+
+class Rank(object):
+  pass
+
+class Vote(object):
+  pass
+
+class User(object):
+  pass
---
+++ model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+class Paper(object):
+  pass
+
+class Paragraph(object):
+  pass
+
+class Sentence(object):
+  pass
+
+class Word(object):
+  pass
+
+class User(object):
+  pass
+
+class Storage(object):
+  pass
