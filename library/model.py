1d5da5a056c22af00d75b1fe026f33305f2583e3
Shawn Lee <dashawn@gmail.com>
2011-12-19 00:24:19 -0800

    Added new data types to the model.

--- library/model.py
+++ library/model.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python
 
-class Paper(object):
+from google.appengine.ext import db
+
+
+class Paper(db.Model):
+  paper = db.ListProperty(db.key)
+  sha1 = db.StringProperty(required=True)
+  score = db.IntegerProperty(required=True)
+
+
+class Paragraph(db.Model):
+  paragraph = db.ListProperty(db.key)
+  sha1 = db.StringProperty(required=True)
+  score = db.IntegerProperty(required=True)
+
+
+class ParagraphComparison(db.Model):
+  group = db.ListProperty(db.key)
+
+
+class Sentence(db.Model):
+  sentence = db.ListProperty(db.key)
+  sha1 = db.StringProperty(required=True)
+  score = db.IntegerProperty(required=True)
+
+
+class SentenceComparison(db.Model):
+  group = db.ListProperty(db.key)
+
+
+class Word(db.Model):
+  word = db.StringProperty(required=True)
+  soundex = db.StringProperty(required=True)
+
+
+class User(db.Model):
+  user_id = db.IntegerProperty(required=True)
+  score = db.IntegerProperty(required=True)
+  user_name = db.StringProperty(required=True)
+  password = db.StringProperty(required=True)
+  nonce = db.StringProperty(required=True)
+  sign_up_date = db.DateTimeProperty(auto_now_add=True)
+  status = db.IntegerProperty(required=True)
+
+
+class Vote(db.Model):
   pass
 
-class Paragraph(object):
-  pass
 
-class Sentence(object):
-  pass
-
-class Word(object):
-  pass
-
-class User(object):
-  pass
-
-class Storage(object):
+class Log(db.Model):
   pass
